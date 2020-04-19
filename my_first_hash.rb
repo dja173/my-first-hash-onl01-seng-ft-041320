dbbaba51024a7a5f64fad3ed66bfa045fe249390
dustin <djarcene@gmail.com>
2020-04-19 17:15:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,7 +20,7 @@ def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3, "muskets" => 2, "gun powder" => 4}
 end
 
-def adding
+def add
   shipping_manifest["pearl necklace"] = 1
   shipping_manifest
 end
